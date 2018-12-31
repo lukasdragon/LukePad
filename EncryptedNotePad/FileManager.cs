9e20d0920ab2cbf35e66f781b56053b43ca3e467
Lukas Olson <lukasdragon@live.ca>
2018-12-31 12:54:35 -0800

    New Skin, and finished file format

--- EncryptedNotePad/FileManager.cs
+++ EncryptedNotePad/FileManager.cs
@@ -1,9 +1,8 @@
 ﻿using EncryptedNotePad.Serialization;
-using System;
 using System.IO;
+using System.IO.Compression;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace EncryptedNotePad
@@ -11,18 +10,37 @@ namespace EncryptedNotePad
     internal class FileManager
     {
 
-        private static readonly byte[] Salt = new byte[] { 15, 30, 35, 40, 55, 68, 69, 80 };
+
 
 
         public string LoadFile(string path, string password, ref System.Drawing.Color TextColor, ref System.Drawing.Font TextFont)
         {
             IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
-            LPF obj = (LPF)formatter.Deserialize(stream);
-            stream.Close();
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Decompress);
+
+
+            LPF obj = (LPF)formatter.Deserialize(compressionStream);
+
+            compressionStream.Close();
+            fileStream.Close();
+
+            //    decompressionStream.Close();
             TextColor = obj.Color;
             TextFont = obj.Font;
-            return DecryptString(obj.Content, CreateKey(password, Salt, 32));
+            return Encoding.UTF8.GetString(AESThenHMAC.SimpleDecryptWithPassword(obj.Content, password, 16));
+        }
+
+
+        private static string LoadCompressedFile(string filename)
+        {
+            FileStream fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Decompress);
+            StreamReader reader = new StreamReader(compressionStream);
+            string data = reader.ReadToEnd();
+            reader.Close();
+            return data;
         }
 
 
@@ -31,64 +49,35 @@ namespace EncryptedNotePad
             LPF obj = new LPF
             {
                 Color = TextColor,
-                Font = TextFont,
-                Content = EncryptString(text, CreateKey(password, Salt, 32))
+                Font = TextFont
             };
-
+            obj.Content = AESThenHMAC.SimpleEncryptWithPassword(Encoding.UTF8.GetBytes(text), password, obj.verificationString);
+                                          
             IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write);
 
-            formatter.Serialize(stream, obj);
-            stream.Close();
+            FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+            GZipStream compressionStream = new GZipStream(fileStream,CompressionMode.Compress);
+            formatter.Serialize(compressionStream, obj);
 
-            return false;
+            compressionStream.Close();
+            fileStream.Close();
+            
+            //   compressionStream.Close();
+
+            return true;
         }
 
-
-
-        public static string EncryptString(string message, byte[] key)
+        private static void SaveCompressedFile(string filename, string data)
         {
-            var aes = new AesCryptoServiceProvider();
-            var iv = aes.IV;
-            using (var memStream = new System.IO.MemoryStream())
-            {
-                memStream.Write(iv, 0, iv.Length);  // Add the IV to the first 16 bytes of the encrypted value
-                using (var cryptStream = new CryptoStream(memStream, aes.CreateEncryptor(key, aes.IV), CryptoStreamMode.Write))
-                {
-                    using (var writer = new System.IO.StreamWriter(cryptStream))
-                    {
-                        writer.Write(message);
-                    }
-                }
-                var buf = memStream.ToArray();
-                return Convert.ToBase64String(buf, 0, buf.Length);
-            }
+            FileStream fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Compress);
+            StreamWriter writer = new StreamWriter(compressionStream);
+            writer.Write(data);
+            writer.Close();
         }
 
-        public static string DecryptString(string encryptedValue, byte[] key)
-        {
-            var bytes = Convert.FromBase64String(encryptedValue);
-            var aes = new AesCryptoServiceProvider();
-            using (var memStream = new System.IO.MemoryStream(bytes))
-            {
-                var iv = new byte[16];
-                memStream.Read(iv, 0, 16);  // Pull the IV from the first 16 bytes of the encrypted value
-                using (var cryptStream = new CryptoStream(memStream, aes.CreateDecryptor(key, iv), CryptoStreamMode.Read))
-                {
-                    using (var reader = new System.IO.StreamReader(cryptStream))
-                    {
-                        return reader.ReadToEnd();
-                    }
-                }
-            }
-        }
+       
 
-        private static byte[] CreateKey(string password, byte[] salt, int keyBytes = 32)
-        {
-            const int Iterations = 300;
-            var keyGenerator = new Rfc2898DeriveBytes(password, Salt, Iterations);
-            return keyGenerator.GetBytes(keyBytes);
-        }
 
 
 
